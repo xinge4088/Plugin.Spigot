1416f5e7103a7ec9e1e050bff8bc8d440160c4b4
xinge4088 <70421200+xinge4088@users.noreply.github.com>
2024-12-21 19:01:37 +0800

    Update QQCommand.java

--- QQBot/src/main/java/org/lonelysail/qqbot/server/commands/QQCommand.java
+++ QQBot/src/main/java/org/lonelysail/qqbot/server/commands/QQCommand.java
@@ -4,19 +4,14 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.lonelysail.qqbot.websocket.WsSender;
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class QQCommand implements CommandExecutor {
     private final String name;
     private final WsSender sender;
-    private final JavaPlugin plugin;  // 插件实例
 
-    // 修改构造函数，传入插件实例
-    public QQCommand(JavaPlugin plugin, WsSender sender, String name) {
+    public QQCommand(WsSender sender, String name) {
         this.name = name;
         this.sender = sender;
-        this.plugin = plugin;  // 保存插件实例
     }
 
     @Override
@@ -25,20 +20,8 @@ public class QQCommand implements CommandExecutor {
             return false;
         }
         String message = String.format("[%s] <%s> %s", this.name, sender.getName(), args[0]);
-
-        // 将发送消息的操作放到异步线程中
-        plugin.getServer().getScheduler().runTaskAsync(plugin, () -> {
-            boolean success = this.sender.sendSynchronousMessage(message);
-            // 使用主线程反馈消息
-            plugin.getServer().getScheduler().runTask(plugin, () -> {
-                if (success) {
-                    sender.sendMessage("§a发送消息成功！");
-                } else {
-                    sender.sendMessage("§c发送消息失败！");
-                }
-            });
-        });
-
+        if (this.sender.sendSynchronousMessage(message)) sender.sendMessage("§a发送消息成功！");
+        else sender.sendMessage("§c发送消息失败！");
         return true;
     }
 }
