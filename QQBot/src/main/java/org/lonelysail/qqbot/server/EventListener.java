2053e2400237f4d6fa40d293333f082f40b33773
xinge4088 <70421200+xinge4088@users.noreply.github.com>
2024-12-21 17:44:25 +0800

    Update EventListener.java

--- QQBot/src/main/java/org/lonelysail/qqbot/server/EventListener.java
+++ QQBot/src/main/java/org/lonelysail/qqbot/server/EventListener.java
@@ -8,6 +8,7 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.lonelysail.qqbot.websocket.WsSender;
+import org.bukkit.Bukkit;
 
 public class EventListener implements Listener {
     private final WsSender sender;
@@ -19,28 +20,38 @@ public class EventListener implements Listener {
     // 当玩家退出游戏时触发
     @EventHandler
     public void PlayerQuit(PlayerQuitEvent event) {
-        // 发送玩家离开游戏的消息
-        this.sender.sendPlayerLeft(event.getPlayer().getName());
+        // 使用异步任务发送玩家离开消息
+        Bukkit.getScheduler().runTaskAsync(YourPluginInstance, () -> {
+            this.sender.sendPlayerLeft(event.getPlayer().getName());
+        });
     }
 
     // 当玩家加入游戏时触发
     @EventHandler
     public void playerJoin(PlayerJoinEvent event) {
-        // 发送玩家加入游戏的消息
-        this.sender.sendPlayerJoined(event.getPlayer().getName());
+        // 使用异步任务发送玩家加入消息
+        Bukkit.getScheduler().runTaskAsync(YourPluginInstance, () -> {
+            this.sender.sendPlayerJoined(event.getPlayer().getName());
+        });
     }
 
     // 当玩家聊天时触发
     @EventHandler
     public void playerChat(AsyncPlayerChatEvent event) {
-        // 发送玩家聊天的消息
-        this.sender.sendPlayerChat(event.getPlayer().getName(), event.getMessage());
+        // 使用异步任务发送玩家聊天消息
+        Bukkit.getScheduler().runTaskAsync(YourPluginInstance, () -> {
+            this.sender.sendPlayerChat(event.getPlayer().getName(), event.getMessage());
+        });
     }
+
+    // 当玩家死亡时触发
     @EventHandler
     public void playerDeath(PlayerDeathEvent event) {
-        // 获取死亡的玩家对象
-        Player player = event.getEntity();
-        // 发送玩家死亡的消息
-        this.sender.sendPlayerDeath(player.getName(), event.getDeathMessage());
+        // 使用异步任务发送玩家死亡消息
+        Bukkit.getScheduler().runTaskAsync(YourPluginInstance, () -> {
+            Player player = event.getEntity();
+            this.sender.sendPlayerDeath(player.getName(), event.getDeathMessage());
+        });
     }
 }
+
