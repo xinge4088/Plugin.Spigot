796be6b9a855a94920aa87fc3e5dea88a6471832
xinge4088 <70421200+xinge4088@users.noreply.github.com>
2024-12-21 17:45:05 +0800

    Update QQCommand.java

--- QQBot/src/main/java/org/lonelysail/qqbot/server/commands/QQCommand.java
+++ QQBot/src/main/java/org/lonelysail/qqbot/server/commands/QQCommand.java
@@ -4,6 +4,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.lonelysail.qqbot.websocket.WsSender;
+import org.bukkit.Bukkit;
 
 public class QQCommand implements CommandExecutor {
     private final String name;
@@ -20,8 +21,21 @@ public class QQCommand implements CommandExecutor {
             return false;
         }
         String message = String.format("[%s] <%s> %s", this.name, sender.getName(), args[0]);
-        if (this.sender.sendSynchronousMessage(message)) sender.sendMessage("§a发送消息成功！");
-        else sender.sendMessage("§c发送消息失败！");
+
+        // 将发送消息的操作放到异步线程中
+        Bukkit.getScheduler().runTaskAsync(YourPluginInstance, () -> {
+            boolean success = this.sender.sendSynchronousMessage(message);
+            // 使用主线程反馈消息
+            Bukkit.getScheduler().runTask(YourPluginInstance, () -> {
+                if (success) {
+                    sender.sendMessage("§a发送消息成功！");
+                } else {
+                    sender.sendMessage("§c发送消息失败！");
+                }
+            });
+        });
+
         return true;
     }
 }
+
